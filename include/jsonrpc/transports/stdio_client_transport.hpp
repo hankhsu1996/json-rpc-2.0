1e7a3ee110ad28ad8f7700fc2b3f71476d3e4096
Shou-Li Hsu <hank850503@gmail.com>
2024-07-30 20:31:24 -0700

    Add missing docs in stdio client transport

--- include/jsonrpc/transports/stdio_client_transport.hpp
+++ include/jsonrpc/transports/stdio_client_transport.hpp
@@ -6,6 +6,9 @@
 
 namespace jsonrpc {
 
+/**
+ * @brief Transport layer using standard I/O for JSON-RPC communication.
+ */
 class StdioClientTransport : public ClientTransport {
 public:
   void SendRequest(const std::string &request) override;
