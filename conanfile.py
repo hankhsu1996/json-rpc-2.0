0d87d504c0cdec900f78953b60f530d17e7262d1
Shou-Li Hsu <hank850503@gmail.com>
2024-08-16 14:32:14 -0700

    Standardize Naming Convention Across Project (#27)

--- conanfile.py
+++ conanfile.py
@@ -2,12 +2,13 @@ from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 
-class JsonRpcConan(ConanFile):
-    name = "json-rpc-2.0"
+class JsonRpcRecipe(ConanFile):
+    """ Conan recipe for jsonrpc-cpp-lib """
+    name = "jsonrpc-cpp-lib"
     version = "1.0.0"
     license = "MIT"
     author = "Shou-Li Hsu <hank850503@gmail.com>"
-    url = "https://github.com/hankhsu1996/json-rpc-2.0"
+    url = "https://github.com/hankhsu1996/jsonrpc-cpp-lib"
     description = (
         "Welcome to the JSON-RPC 2.0 Modern C++ Library! "
         "This library provides a lightweight, modern C++ implementation of "
@@ -37,26 +38,45 @@ class JsonRpcConan(ConanFile):
         "catch2/3.6.0"
     ]
 
-    # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+    # Define options for building examples and tests
+    options = {
+        "build_examples": [True, False],
+        "build_tests": [True, False]
+    }
+    default_options = {
+        "build_examples": False,
+        "build_tests": False
+    }
+
+    exports_sources = "CMakeLists.txt", "src/*", "include/*", "LICENSE", "README.md"
 
     def layout(self):
+        """ Define the layout of the project """
         cmake_layout(self)
 
     def generate(self):
+        """ Generate the CMake toolchain and dependencies files """
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
         tc.user_presets_path = 'ConanPresets.json'
-
+        tc.cache_variables["USE_CONAN"] = "ON"
+        tc.cache_variables["BUILD_EXAMPLES"] = self.options.build_examples
+        tc.cache_variables["BUILD_TESTS"] = self.options.build_tests
         tc.generator = "Ninja"
         tc.generate()
 
     def build(self):
+        """ Build the project """
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def package(self):
+        """ Package the project """
         cmake = CMake(self)
         cmake.install()
+
+    def package_info(self):
+        """ Define package information for consumers """
+        self.cpp_info.libs = ["jsonrpc-cpp-lib"]
